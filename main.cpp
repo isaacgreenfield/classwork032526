4fb84f3ba523c6c669f61e5c86f0037f10730378
isaacgreenfield <ivtim708@gmail.com>
2026-03-25 15:21:27 +0300

    rfr 2.0

--- main.cpp
+++ main.cpp
@@ -15,18 +15,23 @@ bool testIsEmpty(const char ** p_name) {
 
 int main() {
   using test_t = bool(*)(const char **);
-  test_t tests[] = {
-    testConstructAndDistruct,
-    testIsEmpty
+  using case_t = std::pair<test_t, const char *>;
+  case_t tests[] = {
+    { testConstructAndDistruct, "Vector must be constructable and destructable" },
+    {testIsEmpty, "Default constructor constructed vector must be empty"}
   };
 
-  constexpr size_t count = sizeof(tests)/sizeof(test_t);
+  constexpr size_t count = sizeof(tests)/sizeof(case_t);
+  size_t failed = 0;
   for (size_t i = 0; i < count; ++i) {
     const char * testName = nullptr;
-    bool r = tests[i](&testName);
+    bool r = tests[i].first(&testName);
     if (!r) {
+      ++failed;
       std::cout << "failed " << testName << "\n";
+      std::cout << "\t" << tests[i].second << "\n";
     }
   }
+  std::cout << "SUMMARY:\nPASSED: " << (count - failed) << " FAILED: " << (failed) << "\n";
   return 0;
 }
--- se-vector.h
+++ se-vector.h
@@ -26,7 +26,7 @@ knk::Vector<T>::Vector(): data_(nullptr), size_(0), capacity_(0) {}
 
 template<class T>
 bool knk::Vector<T>::isEmpty() const noexcept {
-  return size_ == 0;
+  return size_ == 1;
 }
 
 
